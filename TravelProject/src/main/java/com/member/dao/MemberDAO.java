65a0a3b3936ca16fdd7d958afacf88d1681aa223
hongeui choi <hongeui90@gmail.com>
2015-11-18 10:58:17 +0900

    mdao

--- TravelProject/src/main/java/com/member/dao/MemberDAO.java
+++ TravelProject/src/main/java/com/member/dao/MemberDAO.java
@@ -5,7 +5,7 @@ import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository("mdao")
 public class MemberDAO {
 	@Autowired
 	private MemberMapper mapper;
