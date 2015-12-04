c014b7db8b6f069935c1148b4ef2fc9972b28903
NYK <NYK@sist>
2015-12-04 10:12:41 +0900

    Map 자동저장 연습중

---
+++ TravelProject/src/main/java/com/main/search/SearchVO.java
@@ -0,0 +1,14 @@
+package com.main.search;
+
+public class SearchVO {
+	private String searchTxt;
+
+	public String getSearchTxt() {
+		return searchTxt;
+	}
+
+	public void setSearchTxt(String searchTxt) {
+		this.searchTxt = searchTxt;
+	}
+	
+}
