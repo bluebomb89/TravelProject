c49396342edbce91bdceab94074b4484ecbf7ed3
sist <sist@localhost>
2015-12-17 16:18:22 +0900

    하둡 연동 완료

--- TravelProject/src/main/java/com/sist/main/SearchController.java
+++ TravelProject/src/main/java/com/sist/main/SearchController.java
@@ -4,12 +4,14 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SearchController {
+	@Autowired
 	private SearchManager smr;
 	@RequestMapping("travel_search.tvl")
 	public String travel_search(SearchVO vo,String searchText) throws Exception{
